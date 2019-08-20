4d84d1c9e3c2fe46e6ac9f1b4bbd2987e91b0a9a
Hugo Rafael <hugorafael.alves@hotmail.com>
2019-08-20 08:41:01 -0300

    local storage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,7 @@ class UserController{
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
     }
 
     onEdit(){
@@ -39,21 +40,11 @@ class UserController{
                         result._photo = content;
                     }
                     
-                    tr.dataset.user = JSON.stringify(result);
+                    let user = new User();
 
-                    tr.innerHTML = `
-                        <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${result._name}</td>
-                        <td>${result._email}</td>
-                        <td>${result._admin ? "Sim" : "Não"}</td>
-                        <td>${Utils.dateFormat(result._register)}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>
-                    `;
+                    user.loadFromJSON(result);
 
-                    this.addEventsTr(tr);
+                    this.getTr(user, tr);
 
                     this.updateCount();
 
@@ -85,6 +76,8 @@ class UserController{
             this.getPhoto(this.formEl).then(
                 (content) => {
                     values.photo = content;
+
+                    this.insert(values);
 
                     this.addLine(values);
 
@@ -169,12 +162,52 @@ class UserController{
         );
     }
 
+    getUsersStorage(){
+        let users = [];
+
+        // if(sessionStorage.getItem("users")){
+        if(localStorage.getItem("users")){
+            // users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
+        }
+
+        return users;
+    }
+
+    selectAll(){
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser => {
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+        });
+        
+    }
+
+    insert(data){
+        let users = this.getUsersStorage();
+
+        users.push(data);
+
+        // sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
+    }
+
     addLine(dataUser){
-        let tr = document.createElement('tr');
+        let tr = this.getTr(dataUser);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    getTr(dataUser, tr = null){
+        if(tr === null) tr = document.createElement('tr');
 
         tr.dataset.user = JSON.stringify(dataUser);
-
-        console.log(dataUser);
 
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -190,9 +223,7 @@ class UserController{
 
         this.addEventsTr(tr);
 
-        this.tableEl.appendChild(tr);
-
-        this.updateCount();
+        return tr;
     }
 
     addEventsTr(tr){
